785523ea7872d06c3118563eebf2480461b91dd0
areef44 <areef.empat.empat@gmail.com>
2023-09-16 20:12:26 +0700

    modify entities users domain

--- src/Domains/users/_test/UserRepository.test.js
+++ src/Domains/users/_test/UserRepository.test.js
@@ -7,6 +7,8 @@ describe('UserRepository interface', () => {
 
         // Action and Assert
         await expect(userRepository.addUser({})).rejects.toThrowError('USER_REPOSITORY.METHOD_NOT_IMPLEMENTED');
-        await expect(userRepository.verifyAvailableUsername('')).rejects.toThrowError('USER_REPOSITORY.METHOD_NOT_IMPLEMENTED')
+        await expect(userRepository.verifyAvailableUsername('')).rejects.toThrowError('USER_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+        await expect(userRepository.getPasswordByUsername('')).rejects.toThrowError('USER_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+        await expect(userRepository.getIdByUsername('')).rejects.toThrowError('USER_REPOSITORY.METHOD_NOT_IMPLEMENTED');
     });
 });
